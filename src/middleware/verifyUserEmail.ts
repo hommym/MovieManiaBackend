674b916b5c2da47a95f988bfa3c1867bdfe456ee
herberth arthur <herbertharthur80@gmail.com>
2024-05-11 14:59:14 +0000

    Added a middleware for verifying if an email provided is associated with any account

---
+++ src/middleware/verifyUserEmail.ts
@@ -0,0 +1,35 @@
+import { Request, Response, NextFunction } from "express";
+import { UserSchema } from "../schemas/userSchema";
+import { User } from "../helperToools/customDataTypes";
+
+export const verifyUserEmail = async (req: Request, res: Response, next: NextFunction) => {
+  console.log("A User is been Authenticated...");
+  try {
+    if (!req.body.email) {
+      res.status(400);
+      throw new Error("Bad request invalid request body");
+    }
+
+    // checking if an account with this email exist
+    console.log("User email verification started ...")
+    const accountsInDatabase: Array<User> = await UserSchema.find({ email: req.body.email });
+    if (accountsInDatabase.length === 0) {
+      console.log("No Account with this  email  exist");
+      res.status(409);
+      throw new Error("Invalid email and password");
+    }
+
+    
+    console.log("An account with user email exist")
+
+    // setting id and hashed passwords in the req.body for later use in the next middleware
+    req.body.id = String(accountsInDatabase[0]._id);
+    req.body.hashedPassword = accountsInDatabase[0].password
+
+    // moving to the next middleware
+    next();
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+};
